675a12c8193a5d5561585031ef3a6a68ea40f8a3
yoana.quintero <yoana.quintero@codesa.com.co>
2025-08-27 11:29:35 -0500

    Adds CD job e instalacion de argcd

--- src/app.py
+++ src/app.py
@@ -19,4 +19,4 @@ if __name__ == '__main__':
 
     app.run(host="0.0.0.0")
 
-#Aplicacion Python con runner con dependnecia e instalaccion de argocd :) :)!
+#Aplicacion Python con runner con dependnecia e instalaccion de argocd :)!
